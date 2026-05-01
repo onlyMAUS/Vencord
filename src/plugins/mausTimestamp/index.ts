c06022f0e81807388872091cc5c233789ce4f956
onlyMAUS <ptt.mau@gmail.com>
2026-05-01 11:38:03 +0200

    Update index.ts

--- src/plugins/mausTimestamp/index.ts
+++ src/plugins/mausTimestamp/index.ts
@@ -1,26 +1,16 @@
-import { definePlugin } from "../../api/plugins";
-import { patcher, webpack } from "../../api";
-
-export default definePlugin({
-    name: "MausTimestamp",
-    description: "Mostra la data nel formato DD MM YY | HH : MM : SS",
-    authors: [{ name: "onlyMAUS", id: 1106518428453474324n }], // Ho inserito un ID d'esempio, puoi mettere il tuo
-    tags: ["timestamp", "custom"],
-
-    start() {
-        const MessageTimestamp = webpack.findByProps("MessageTimestamp");
+start() {
+        // Usiamo un finder più robusto per trovare il componente del Timestamp
+        const MessageTimestamp = webpack.findByDisplayName("MessageTimestamp", false);
 
         if (MessageTimestamp) {
-            patcher.after(MessageTimestamp, "default", (_, res) => {
+            patcher.after(MessageTimestamp, "default", (args, res) => {
+                // Verifichiamo che il timestamp esista nel componente
                 if (res?.props?.timestamp) {
-                    // Il formato richiesto: %d %m %y | %H : %M : %S
+                    // Sovrascriviamo la funzione di rendering o il testo direttamente
+                    // Il formato richiesto: DD - MM - YY | HH : mm : ss
                     res.props.timestamp = res.props.timestamp.format("DD - MM - YY | HH : mm : ss");
                 }
+                return res;
             });
         }
     },
-
-    stop() {
-        patcher.unpatchAll();
-    }
-});
